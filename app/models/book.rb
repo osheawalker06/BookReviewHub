bb2a08b8e5240afd11b5351857671de45abeabd3
Oshea Walker <osheawalker06@gmail.com>
2026-05-21 08:47:44 -0500

    Review model sucessfully added and associations have been made.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   validates :title, presence: true
   validates :author, presence: true
   validates :description, presence: true
