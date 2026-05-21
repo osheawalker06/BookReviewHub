936f747cb47b07332251682eeca97ef51ced13f6
Oshea Walker <osheawalker06@gmail.com>
2026-05-21 08:00:29 -0500

    Book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :genre, presence: true
+  validates :isbn, presence: true, uniqueness: true
+  validates :cover_url, presence: true
 end
