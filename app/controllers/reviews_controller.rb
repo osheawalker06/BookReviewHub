7a435bf6d95a831288268a87d6589f6c6a6e3130
Oshea Walker <osheawalker06@gmail.com>
2026-05-21 09:57:41 -0500

    CRUD for review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,44 @@
+class ReviewsController < ApplicationController
+  before action: set_book
+  before action: set_review, only: [ :edit, :update, :destroy ]
+
+  def create
+    @review = @book.reviews.build(review_params)
+    if @review.save
+      redirect_to @book, notice: "Review has been created."
+    else
+      @reviews = @book.reviews.where.not(id: nil).order(created_at: :desc)
+      render "books/show", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @book, notice: "Review has been updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to @book, notice: "Review deleted"
+  end
+
+  private
+
+  def set_review
+    @review = @book.reviews.find(params[:id])
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+end
