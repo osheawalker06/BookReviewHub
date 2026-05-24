ffeecea8611e0704c7678101cdc3845e1d212395
Oshea Walker <osheawalker06@gmail.com>
2026-05-24 01:46:18 -0500

    Added User Authentication

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,18 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!,  only: [ :create, :edit, :update, :destroy ]
   before_action :set_book
   before_action :set_review, only: [ :edit, :update, :destroy ]
+  before_action :authorize_reviewer!, only: [ :edit, :update, :destroy ]
+
 
   def create
     @review = @book.reviews.build(review_params)
+    @review.user = current_user
+
     if @review.save
       redirect_to @book, notice: "Review has been created."
     else
-      @reviews = @book.reviews.where.not(id: nil).order(created_at: :desc)
+      @reviews = @book.reviews.reload
       render "books/show", status: :unprocessable_entity
     end
   end
@@ -36,6 +41,12 @@ class ReviewsController < ApplicationController
 
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def authorize_reviewer!
+    unless @review.user == current_user
+      redirect_to @book, alert: "You are not authorized to alter this review."
+    end
   end
 
   def review_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_21_132741) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_24_052311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,8 +31,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_21_132741) do
     t.datetime "created_at", null: false
     t.integer "rating"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
